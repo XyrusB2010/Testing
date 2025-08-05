064b14378dd8d081de1200f32b106c11f38296a1
Xyrus Babao <xyrus_babao@paraparaumucollege.school.nz>
2025-08-05 00:34:59 +0000

    Update sha256.py

--- Python/sha256.py
+++ Python/sha256.py
@@ -15,6 +15,17 @@ def decToBin(decimal):
         bin_list = '0' + bin_list
     return bin_list
 
+def rotr(input, bits):
+    return ((input >> bits) | (input << (32 - bits))) & 0xFFFFFFFF
+def shiftr(input, bits):
+    return input >> bits
+def sigma0(n):
+    return rotr(n, 7) ^ rotr(n, 18) ^ shiftr(n, 3)
+def sigma1(n):
+    return rotr(n, 17) ^ rotr(n, 19) ^ shiftr(n, 10)
+def add32(*args):
+    return sum(args) & 0xFFFFFFFF
+
 h0 = 0x6a09e667
 h1 = 0xbb67ae85
 h2 = 0x3c6ef372
@@ -24,6 +35,27 @@ h5 = 0x9b05688c
 h6 = 0x1f83d9ab
 h7 = 0x5be0cd19
 
+K = [
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+]
+
+a, b, c, d, e, f, g, h = h0, h1, h2, h3, h4, h5, h6, h7
+
 message = input('Enter message to encrypt with SHA256: ')
 message = ''.join(format(ord(char), '08b') for char in message)
 messageLen = len(message)
@@ -32,4 +64,51 @@ message += '1'
 while len(message) < 448:
     message += '0'
 message += decToBin(messageLen)
-print(len(message))
+
+slicedMessage = [(message[i:i+32]) for i in range(0, len(message), 32)]
+W = [int(word, 2) for word in slicedMessage]
+
+for x in range(16, 64):
+    word = add32(
+        sigma1(W[x - 2]),
+        W[x - 7],
+        sigma0(W[x - 15]),
+        W[x - 16]
+    )
+    W.append(word)
+
+for i in range(64):
+    S1 = rotr(e, 6) ^ rotr(e, 11) ^ rotr(e, 25)
+    ch = (e & f) ^ ((~e) & g)
+    temp1 = add32(h, S1, ch, K[i], W[i])
+    S0 = rotr(a, 2) ^ rotr(a, 13) ^ rotr(a, 22)
+    maj = (a & b) ^ (a & c) ^ (b & c)
+    temp2 = add32(S0, maj)
+
+    h = g
+    g = f
+    f = e
+    e = add32(d, temp1)
+    d = c
+    c = b
+    b = a
+    a = add32(temp1, temp2)
+
+h0 = add32(h0, a)
+h1 = add32(h1, b)
+h2 = add32(h2, c)
+h3 = add32(h3, d)
+h4 = add32(h4, e)
+h5 = add32(h5, f)
+h6 = add32(h6, g)
+h7 = add32(h7, h)
+
+output = ''.join(format(x, '08x') for x in [h0, h1, h2, h3, h4, h5, h6, h7])
+print('Output: ', output)
+
+saveHash = input('Save hash to output file? (y/N): ') == 'y'
+if saveHash:
+    filename = input('Enter filename: ') + '.sha256sum'
+    with open(filename, 'w') as file:
+        file.write(output)
+    print(f'Hash saved to file: {filename}')
