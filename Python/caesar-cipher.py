503f526635b980e3a1d233caf387f41068179b9c
Xyrus Babao <xyrus_babao@paraparaumucollege.school.nz>
2026-05-07 00:48:44 +0000

    Add fully decrypted phrase

--- Python/caesar-cipher.py
+++ Python/caesar-cipher.py
@@ -52,10 +52,11 @@ def solve():
     for i in range(len(wordList)):
         wordList[i] = wordList[i].upper()
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    # numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     encrypted = input('Enter message to solve: ').upper()
     encryptedList = encrypted.split()
     attempts = []
+    decryptedList = []
 
     for word in encryptedList:
         attempts = []
@@ -78,12 +79,15 @@ def solve():
             attempts.append(''.join(decrypted))
         for attempt in attempts:
             if attempt in wordList:
+                decryptedList.append(attempt)
                 print(f'Match found: {attempt}')
                 print(f'Number of shifts used: {attempts.index(attempt)}')
                 print(f'Other possibilities: {attempts} \n')
                 foundMatch = True
         if foundMatch != True:
                 print(f'No match found, but here are all possibilities: {attempts} \n')
+
+    print(f'Fully decoded phrase: {' '.join(decryptedList)}')
 
 import sys
 option = ''
@@ -94,5 +98,5 @@ if option == 1: encrypt()
 elif option == 2: decrypt()
 elif option == 3: solve()
 else:
-    print(f'Enter "1", "2", or "3"! Not {option}!', file=sys.stderr)
+    print(f'Enter "1", "2" or "3"! Not {option}!', file=sys.stderr)
     exit()
